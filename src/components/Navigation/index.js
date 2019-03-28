f4387e0b6f471a4a98eadff07293d5442e85d995
Dan Groze <dangroze@gmail.com>
2019-03-28 00:08:45 +0000

    :rotating_light:More linter refactoring

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './navigation.css';
-import * as ROUTES from '../../constants/routes';
-import SignOutButton from '../SignOut';
-import { AuthUserContext } from '../Session';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navigation.css";
+import * as ROUTES from "../../constants/routes";
+import SignOutButton from "../SignOut";
+import { AuthUserContext } from "../Session";
 
 const Navigation = () => (
-<nav className="nav-bar" role="navigation" aria-label="main navigation">
+  <nav className="nav-bar" role="navigation" aria-label="main navigation">
     <AuthUserContext.Consumer>
-      {authUser =>
-        authUser ? <NavigationAuth /> : <NavigationNonAuth />
-      }
+      {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
     </AuthUserContext.Consumer>
   </nav>
 );
 
 const NavigationAuth = () => (
-  <nav className = "columns">
+  <nav className="columns">
     <ul className="button is-small button is-info is-outlined">
       <Link to={ROUTES.HOME}>What Zen</Link>
     </ul>
@@ -30,8 +28,7 @@ const NavigationAuth = () => (
 );
 
 const NavigationNonAuth = () => (
-
-  <nav className = "columns">
+  <nav className="columns">
     <ul className="button is-small button is-info is-outlined">
       <Link to={ROUTES.LANDING}>What Zen</Link>
     </ul>
--- src/components/NewCardForm/index.js
+++ src/components/NewCardForm/index.js
@@ -1,70 +1,73 @@
-import React, { Component } from 'react';
-import app from 'firebase/app';
-
+import React, { Component } from "react";
+import app from "firebase/app";
 
 class NewCardForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.updateInput = this.updateInput.bind(this);
     this.addCard = this.addCard.bind(this);
     this.state = {
-      title: '',
-      details: '',
-      status: 'todo',
-      comments: '',
-      user: '',
+      title: "",
+      details: "",
+      status: "todo",
+      comments: "",
+      user: "",
       important: false,
       urgent: false,
-      timeCreated: '',
-    }
+      timeCreated: ""
+    };
   }
 
-  updateInput(e){
+  updateInput(e) {
     e.preventDefault();
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  addCard(e){
+  addCard(e) {
     e.preventDefault();
     let fullDate = Date(Date.now());
     fullDate = fullDate.toString();
     let date = fullDate.split(" ");
-    date = `on ${date[2]}/${date[1]}/${date[3]} at ${date[4]}`
+    date = `on ${date[2]}/${date[1]}/${date[3]} at ${date[4]}`;
 
-    app.database().ref('/cards').push({
-      title: this.state.title,
-      status: 'To do',
-      user: this.props.useremail,
-      details: '',
-      comments: '',
-      important: false,
-      urgent: false,
-      timeCreated: date,
-    });
+    app
+      .database()
+      .ref("/cards")
+      .push({
+        title: this.state.title,
+        status: "To do",
+        user: this.props.useremail,
+        details: "",
+        comments: "",
+        important: false,
+        urgent: false,
+        timeCreated: date
+      });
     this.setState({
-      title: '',
-      status: 'To do',
+      title: "",
+      status: "To do",
       user: this.props.useremail,
-      details: '',
-      comments: '',
+      details: "",
+      comments: "",
       important: false,
       urgent: false,
-      timeCreated: date,
+      timeCreated: date
     });
   }
   render() {
     return (
       <div className="NewCardForm">
-        <form action="#" onSubmit={this.addCard} >
-          <div><input
-            required
-            className="input"
-            name="title"
-            onChange={this.updateInput}
-            type="text"
-            placeholder="Enter a new task here"
-            value={this.state.title}
-          />
+        <form action="#" onSubmit={this.addCard}>
+          <div>
+            <input
+              required
+              className="input"
+              name="title"
+              onChange={this.updateInput}
+              type="text"
+              placeholder="Enter a new task here"
+              value={this.state.title}
+            />
           </div>
         </form>
       </div>
